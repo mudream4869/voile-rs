bbd67e1d4b728de3f32b81e555f813ef85d309e9
Mudream <mudream4869@gmail.com>
2023-02-06 23:53:03 +0800

    Book cover

--- frontend/src/components/BooksPage.vue
+++ frontend/src/components/BooksPage.vue
@@ -1,16 +1,34 @@
 <template>
   <v-app>
-    <v-card elevation="2" v-for="book in books" :key="book.book_id" outlined shaped class="ma-md-2">
-      <v-card-title>
-        {{ book.title }}
-      </v-card-title>
+    <v-container fluid>
+      <v-row dense>
+        <v-col v-for="book in books" :key="book.book_id" :cols="4">
+          <v-card outlined shaped class="mx-auto ma-md-2" width="400">
+            <v-img
+              v-if="book.book_cover"
+              class="align-end text-white"
+              height="200"
+              :src="content_book_cover(book.book_id)"
+              cover>
+              <v-card-title>
+                {{ book.title }}
+              </v-card-title>
+            </v-img>
+            <v-card-title v-else>
+              {{ book.title }}
+            </v-card-title>
 
-      <v-card-actions>
-        <v-btn outlined :to="{ name: 'book', params: { book_id: book.book_id }}">
-          閱讀
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+            <v-card-actions>
+              <v-btn outlined :to="{ name: 'book', params: { book_id: book.book_id }}">
+                閱讀
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    
   </v-app>
 </template>
 
@@ -28,6 +46,10 @@
     methods: {
       async fetchData() {
         this.books = (await (await fetch('/api/books')).json()).books
+      },
+
+      content_book_cover(book_id) {
+        return `/api/books/${book_id}/contents/0`
       },
     },
   }
