88ccd2b4fd39e8e48e8574eb661dfd3cdc1ccda4
Mudream <mudream4869@gmail.com>
2023-06-14 23:23:59 +0800

    Refine nav

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer">
+    <v-navigation-drawer expand-on-hover rail>
       <v-list>
-        <v-list-item prepend-avatar="/api/user/avatar"
-          :title="user_config.name ? user_config.name : '[未填名字]'"></v-list-item>
+        <v-list-item prepend-avatar="/api/user/avatar" :title="user_config.name ? user_config.name : '[未填名字]'"
+          :to="'/config'" value="user_config"></v-list-item>
       </v-list>
       <v-divider></v-divider>
 
@@ -11,26 +11,22 @@
         <v-list-item :to="'/'" prepend-icon="mdi-home" title="首頁" value="homepage"></v-list-item>
         <v-list-group value="books_group">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" :to="'/books'" prepend-icon="mdi-folder" title="書籍" value="books">
+            <v-list-item v-bind="props" prepend-icon="mdi-folder" title="書籍" value="books">
             </v-list-item>
           </template>
+          <v-list-item prepend-icon="mdi-book" :to="'/books'" title="所有分類" value="books_all">
+          </v-list-item>
           <v-list-item v-for="book_type in books_types" :to="{ name: 'books', query: { book_type } }"
             prepend-icon="mdi-book" :title="book_type" :value="'books_' + book_type">
           </v-list-item>
-          <v-list-item :to="{ name: 'books', query: { book_type: '' } }" title="無分類" value="books_no_type">
+          <v-list-item prepend-icon="mdi-book" :to="{ name: 'books', query: { book_type: '' } }" title="無分類"
+            value="books_no_type">
           </v-list-item>
         </v-list-group>
         <v-list-item :to="'/add_book'" prepend-icon="mdi-plus" title="新增書籍" value="add_book"></v-list-item>
-        <v-list-item :to="'/books_manage'" prepend-icon="mdi-widgets" title="書籍管理" value="books_manage"></v-list-item>
-        <v-list-item :to="'/config'" prepend-icon="mdi-widgets" title="個人設定" value="config"></v-list-item>
+        <v-list-item :to="'/books_manage'" prepend-icon="mdi-details" title="書籍管理" value="books_manage"></v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-app-bar app density="compact">
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-
-      <v-toolbar-title> 我的圖書館 </v-toolbar-title>
-    </v-app-bar>
 
     <v-main>
       <router-view></router-view>
@@ -45,7 +41,6 @@ import { getAllTypes } from './api/books'
 
 export default {
   data: () => ({
-    drawer: null,
     user_config: {
       name: '',
       theme: 'light',
