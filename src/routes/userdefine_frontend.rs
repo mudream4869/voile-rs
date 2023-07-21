a2c230bea82bf9bafab47320ce556b144c2dcc1c
Mudream <mudream4869@gmail.com>
2023-07-21 21:50:34 +0800

    Refine

--- src/routes/userdefine_frontend.rs
+++ src/routes/userdefine_frontend.rs
@@ -1,27 +1,27 @@
 use actix_web::web;
 
 struct UserFrontend {
-    frontend_dir: String,
+    frontend_dir: std::path::PathBuf,
 }
 
 #[actix_web::get("/")]
 async fn index(data: web::Data<UserFrontend>) -> std::io::Result<actix_files::NamedFile> {
-    let index_path: std::path::PathBuf = [&data.frontend_dir, "index.html"].iter().collect();
+    let index_path = data.frontend_dir.join("index.html");
     Ok(actix_files::NamedFile::open(index_path)?)
 }
 
 #[actix_web::get("/favicon.ico")]
 async fn favicon(data: web::Data<UserFrontend>) -> std::io::Result<actix_files::NamedFile> {
-    let favicon_path: std::path::PathBuf = [&data.frontend_dir, "favicon.ico"].iter().collect();
+    let favicon_path = data.frontend_dir.join("favicon.ico");
     Ok(actix_files::NamedFile::open(favicon_path)?)
 }
 
 pub fn configure(cfg: &mut web::ServiceConfig, frontend_dir: &str) {
-    let assets_path: std::path::PathBuf = [frontend_dir, "assets"].iter().collect();
-
     let user_frontend = UserFrontend {
-        frontend_dir: frontend_dir.to_string(),
+        frontend_dir: frontend_dir.into(),
     };
+
+    let assets_path = user_frontend.frontend_dir.join("assets");
 
     cfg.app_data(web::Data::new(user_frontend))
         .service(index)
