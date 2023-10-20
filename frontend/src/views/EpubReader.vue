641f4afd7fcb6d8de5e4cfd054730408c6eaddc5
Mudream <mudream4869@gmail.com>
2023-10-20 23:46:18 +0800

    Remove unused vars

--- frontend/src/views/EpubReader.vue
+++ frontend/src/views/EpubReader.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app style='height: 100vh'>
-    <vue-reader :url="content_src_url" />
+    <vue-reader :url="content_src_url" :epubInitOptions='{ openAs: "epub" }' />
   </v-app>
 </template>
 
@@ -18,21 +18,13 @@ export default {
     return {
       book: {
         title: '',
-        content_titles: [],
       },
 
       content_idx: 0,
       book_id: '',
-
-      // paging in [0, paging_max)
-      paging: 0,
-      paging_max: 1,
     }
   },
   computed: {
-    content_title() {
-      return this.book.content_titles[this.content_idx]
-    },
     content_src_url() {
       return getContentURL(this.book_id, this.content_idx)
     },
@@ -40,13 +32,10 @@ export default {
   created() {
     const route = useRoute()
     this.content_idx = parseInt(route.params.content_idx)
-    this.paging = parseInt(route.params.paging)
     this.book_id = route.params.book_id
 
     // fetch on init
     this.fetchBook()
-
-    setBookProc(this.book_id, this.content_idx, this.paging)
   },
   updated() {
     const new_content_idx = parseInt(this.$route.params.content_idx);
@@ -59,27 +48,13 @@ export default {
     async fetchBook() {
       this.book = await getBook(this.book_id)
     },
-    updatePaging(nextPaging) {
-      if (nextPaging) {
-        this.paging = nextPaging
-      }
-      this.$router.push({
-        name: 'epub_reader',
-        params: {
-          book_id: this.book_id,
-          content_idx: this.content_idx,
-          paging: this.paging,
-        }
-      })
-      setBookProc(this.book_id, this.content_idx, this.paging)
-    },
     async UpdateContentIDX() {
       this.$router.push({
         name: 'pdf_reader',
         params: {
           book_id: this.book_id,
           content_idx: this.content_idx,
-          paging: this.paging,
+          paging: 0,
         }
       })
 
