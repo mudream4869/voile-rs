fa8d0868f6d824d633151ec7efd8a99d7656c312
Mudream <mudream4869@gmail.com>
2024-01-24 19:05:22 +0800

    Refine layout

--- frontend/src/views/Layout.vue
+++ frontend/src/views/Layout.vue
@@ -1,30 +1,45 @@
 <template>
   <v-app>
-    <v-navigation-drawer expand-on-hover rail>
-      <v-list>
-        <v-list-item prepend-avatar="api/config/user/avatar" :title="user_config.name ? user_config.name : '[未填名字]'"
-          :to="'/config'" value="user_config"></v-list-item>
-      </v-list>
-      <v-divider></v-divider>
+    <v-app-bar>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-avatar image="api/config/user/avatar">
+      </v-avatar>
 
+      <v-app-bar-title to="/">
+        {{ user_config.name ? user_config.name : '[未填名字]' }}
+      </v-app-bar-title>
+
+      <v-spacer></v-spacer>
+
+      <v-btn icon to="/add_book">
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+
+      <v-btn icon to="/config">
+        <v-icon>mdi-cog</v-icon>
+      </v-btn>
+
+    </v-app-bar>
+
+    <v-navigation-drawer v-model="drawer">
       <v-list density="compact" nav>
         <v-list-item :to="'/'" prepend-icon="mdi-home" title="首頁" value="homepage"></v-list-item>
         <v-list-group value="books_group">
           <template v-slot:activator="{ props }">
-            <v-list-item v-bind="props" prepend-icon="mdi-folder" title="書籍" value="books">
+            <v-list-item v-bind="props" prepend-icon="mdi-folder" title="書籍分類" value="books">
             </v-list-item>
           </template>
           <v-list-item prepend-icon="mdi-book" :to="'/books'" title="所有分類" value="books_all">
           </v-list-item>
           <v-list-item v-for="book_type in books_types" :to="{ name: 'books', query: { book_type } }"
-            prepend-icon="mdi-book" :title="book_type" :value="'books_' + book_type">
+            prepend-icon="mdi-book" :title="book_type" :value="'books_' + book_type" :key="book_type">
           </v-list-item>
           <v-list-item prepend-icon="mdi-book" :to="{ name: 'books', query: { book_type: '' } }" title="無分類"
             value="books_no_type">
           </v-list-item>
         </v-list-group>
-        <v-list-item :to="'/add_book'" prepend-icon="mdi-plus" title="新增書籍" value="add_book"></v-list-item>
-        <v-list-item :to="'/books_manage'" prepend-icon="mdi-details" title="書籍管理" value="books_manage"></v-list-item>
+        <v-list-item :to="'/books_manage'" prepend-icon="mdi-view-list-outline" title="書籍管理"
+          value="books_manage"></v-list-item>
       </v-list>
     </v-navigation-drawer>
 
@@ -47,6 +62,7 @@ export default {
       theme: 'light',
     },
     books_types: [],
+    drawer: true,
   }),
   methods: {
     async fetchUserConfig() {
